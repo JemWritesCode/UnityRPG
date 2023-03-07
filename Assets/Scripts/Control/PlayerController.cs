747ebc296caf82f3f3a6160153fdb07f24adb499
Jem Cope <thejemmeh@gmail.com>
2023-03-07 06:19:50 -0800

    25 Organizing with namespaces for dependencies

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,21 +1,25 @@
 ﻿using UnityEngine;
+using RPG.Movement;
 
-public class PlayerController : MonoBehaviour {
-
-    private void Update() {
-            if (Input.GetMouseButton(0))
-            {
-            MoveToCursor();
-            }
-    }
-
-    private void MoveToCursor(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if (hasHit)
+namespace RPG.Control{
+    public class PlayerController : MonoBehaviour 
+    {
+        private void Update() 
         {
-            GetComponent<Mover>().MoveTo(hit.point);
+                if (Input.GetMouseButton(0))
+                {
+                MoveToCursor();
+                }
         }
-    }
+
+        private void MoveToCursor(){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            bool hasHit = Physics.Raycast(ray, out hit);
+            if (hasHit)
+            {
+                GetComponent<Mover>().MoveTo(hit.point);
+            }
+        }
+    }   
 }
