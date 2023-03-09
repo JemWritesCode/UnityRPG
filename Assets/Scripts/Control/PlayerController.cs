dac888988e6f0b7a55764a6d49f10b9c9fba963e
Jem Cope <thejemmeh@gmail.com>
2023-03-08 23:46:21 -0800

    26 Raycasting for Components. Clicking on an enemy will raycast even through other objects by using RaycastAll. Can see it printed in console.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -1,25 +1,51 @@
 ﻿using UnityEngine;
 using RPG.Movement;
+using System;
+using RPG.Combat;
 
 namespace RPG.Control{
     public class PlayerController : MonoBehaviour 
     {
         private void Update() 
         {
-                if (Input.GetMouseButton(0))
-                {
-                MoveToCursor();
-                }
+            InteractWithCombat();
+            InteractWithMovement();
         }
 
-        private void MoveToCursor(){
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        private void InteractWithCombat()
+        {
+            RaycastHit[] hits = Physics.RaycastAll(GetMouseRay());
+            foreach(RaycastHit hit in hits)
+            {
+                CombatTarget target = hit.transform.GetComponent<CombatTarget>();
+                if (target == null) continue; 
+                if (Input.GetMouseButtonDown(0))
+                {
+                    GetComponent<Fighter>().Attack(target);
+                }
+            }
+        }
+
+        private void InteractWithMovement(){
+            if (Input.GetMouseButton(0))
+            {
+            MoveToCursor();
+            }
+        }
+
+        private void MoveToCursor()
+        {
             RaycastHit hit;
-            bool hasHit = Physics.Raycast(ray, out hit);
+            bool hasHit = Physics.Raycast(GetMouseRay(), out hit);
             if (hasHit)
             {
                 GetComponent<Mover>().MoveTo(hit.point);
             }
+        }
+
+        private static Ray GetMouseRay()
+        {
+            return Camera.main.ScreenPointToRay(Input.mousePosition);
         }
     }   
 }
