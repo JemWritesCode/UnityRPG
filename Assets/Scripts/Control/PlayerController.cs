ef7c66098472769ac8fad2bf9fc2259c098b5796
Jem Cope <thejemmeh@gmail.com>
2023-03-13 19:58:57 -0700

    29 cancelling combat with movement

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -35,7 +35,7 @@ namespace RPG.Control{
             if (hasHit)
             {
                 if (Input.GetMouseButton(0)){
-                    GetComponent<Mover>().MoveTo(hit.point);
+                    GetComponent<Mover>().StartMoveAction(hit.point);
                 }
                 return true;
             }
