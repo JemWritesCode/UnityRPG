ee81fcd125b364be850c9ce08eb7701490de3e55
Jem Cope <thejemmeh@gmail.com>
2023-03-24 00:03:23 -0700

    40 Stop Attacking after they die or when you move away

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -29,6 +29,7 @@ namespace RPG.Movement{
 
         public void MoveTo(Vector3 destination)
         {
+            
             navMeshAgent.destination = destination;
             navMeshAgent.isStopped = false;
         }
